149eae3a8574c12ef9b7e986cd7598d43f7c85b7
Aanye <spthom+aanye@gmail.com>
2010-04-26 20:25:00 -0700

    Initial commit

---
+++ Locales/enUS.lua
@@ -0,0 +1,23 @@
+
+local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Aanye_XP", "enUS", true)
+if not L then return end
+
+L["Aanye_XP"] = true
+-- LDB Display
+L["Level"] = true
+L["No Faction Selected"] = true
+-- Tooltip: Experience Mode
+L["Experience Summary"] = true
+L["Character is currently at maximum possible level."] = true
+L["XP gains are disabled for this Character."] = true
+L["Current Experience:"] = true
+L["Rest:"] = true
+L["Remaining to Level:"] = true
+-- Tooltip: Reputation Mode
+L["Reputation Summary"] = true
+L["To track a faction reputation, select a faction from the"] = true
+L["Reputation window and check \34Show as Experience Bar\34."] = true
+L["Current Reputation:"] = true
+L["Remaining to X:"] = function(X) return "Remaining to "..X..":"; end
+-- Byline, because I'm so full of myself
+--L["By Aanye on Cenarius (US)"] = true
